9a1c52bb62ded5b4135eaef0b2d6c97bbb7cfd10
zhangfuli <1171067930@qq.com>
2016-07-08 21:26:07 +0800

    no message

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name frontedApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the frontedApp
+ */
+angular.module('frontedApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name frontedApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the frontedApp
+ */
+angular.module('frontedApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
