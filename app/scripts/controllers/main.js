279124b3a302c40cd4f82a97a203145cb9edb34f
zhangfuli <1171067930@qq.com>
2016-07-10 23:04:09 +0800

    首页前端

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -14,4 +14,35 @@ angular.module('frontedApp')
       'AngularJS',
       'Karma'
     ];
+  })
+  .controller('ShopListCtrl',function($scope){
+      $scope.shops=[
+        {
+          'id':'1',
+          'title':'麦琪的礼物',
+          'master':'master1',
+          'imgsrc':'images/touxiang1.jpg'
+        },
+        {
+          'id':'2',
+          'title':'shop22222222222222222222222',
+          'master':'master22222222222222222222',
+          'imgsrc':'images/touxiang2.jpg'
+        }
+        ,
+        {
+          'id':'2',
+          'title':'shop3',
+          'master':'master3',
+          'imgsrc':'2222'
+        }
+        ,
+        {
+          'id':'4',
+          'title':'shop4',
+          'master':'master4',
+          'imgsrc':'3333'
+        }
+      ]
   });
+
