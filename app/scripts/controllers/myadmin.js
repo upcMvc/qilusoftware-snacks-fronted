d2dc72b7bf1768443db68b842919636c82440507
zhangfuli <1171067930@qq.com>
2016-09-08 17:06:08 +0800

    js上传图片显示

--- app/scripts/controllers/myadmin.js
+++ app/scripts/controllers/myadmin.js
@@ -7,6 +7,52 @@
  * # MyadminCtrl
  * Controller of the frontedApp
  */
+!function (a) {
+  function e(n) {
+    if (i[n])return i[n].exports;
+    var t = i[n] = {exports: {}, id: n, loaded: !1};
+    return a[n].call(t.exports, t, t.exports, e), t.loaded = !0, t.exports
+  }
+
+  var i = {};
+  return e.m = a, e.c = i, e.p = "", e(0)
+}([function (a, e, i) {
+  i(1), function (a) {
+    a.fn.jDialog = function (e) {
+      return this.each(function () {
+        function i() {
+          n.allowOverlay && o.get(0).remove(), t.removeClass("show")
+        }
+
+        var n = a.extend({
+          skinClassName: "",
+          animationType: "fade-in",
+          allowOverlay: !0
+        }, e), t = a(this), l = a('<div class="jDialog-content"></div>').addClass(n.skinClassName).append(t.html());
+        if (t.empty().append(l).addClass(n.animationType), n.allowOverlay) {
+          var o = a('<div class="jDialog-overlay"></div>');
+          o.click(i)
+        }
+        t.find('[data-dismiss="JDialog"]').click(i);
+        var s = a('[data-toggle="JDialog"][data-target=' + t.attr("id") + "]");
+        s.click(function () {
+          n.allowOverlay && a("body").append(o), t.addClass("show")
+        })
+      })
+    }
+  }(jQuery)
+}, function (a, e) {
+}]);
+window.onload = function(){
+  $("#dialog-4").jDialog({
+    skinClassName: 'demo',
+    animationType: 'flip',
+    allowOverlay: false
+  });
+  $("#input-id").fileinput();
+  $("#input-id").fileinput({'showUpload':false, 'previewFileType':'any'});
+
+};
 angular.module('frontedApp')
   .controller('MyadminCtrl', function ($scope,$http,$routeParams) {
     $scope.shop={
@@ -60,18 +106,4 @@ angular.module('frontedApp')
         console.log("error");
       });
     };
-    $scope.add = function(){
-      $.confirm({
-        title: 'Confirm!',
-        content: 'Simple confirm!',
-        columnClass: 'col-md-6 col-md-offset-3',
-        container:'body',
-        confirm: function(){
-          $.alert('Confirmed!');
-        },
-        cancel: function(){
-          $.alert('Canceled!')
-        }
-      });
-    }
   });
